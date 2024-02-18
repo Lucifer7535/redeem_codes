d5597880d7f5e871dd02c50cb433fa2f207f6d31
Pranay Pawar <pawarpranay749@gmail.com>
2024-02-18 17:11:09 +0530

    fix: used only euro_gamer for codes

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import asyncio
-import json
 from typing import List
 
 import aiohttp
@@ -7,89 +6,6 @@ from bs4 import BeautifulSoup
 from fastapi import FastAPI
 
 app = FastAPI()
-
-
-async def get_code_from_gamesrader() -> List[str]:
-    try:
-        async with aiohttp.ClientSession() as session, session.get(
-            "https://www.gamesradar.com/genshin-impact-codes-redeem/"
-        ) as response:
-            json_content = await response.text()
-            soup = BeautifulSoup(json_content, "html.parser")
-            lis = soup.select(
-                ".article .text-copy b, .article .text-copy strong, .news-article .text-copy b, .news-article .text-copy strong, .review-article .text-copy b, .review-article .text-copy strong, .static-article .text-copy b, .static-article .text-copy strong" # noqa
-            )
-            codes = [li.text.strip() for li in lis if li.text.strip().isupper()] # noqa
-    except Exception as e:
-        print(f"Error in get_code_from_gamesrader: {e}")
-        return []
-
-    return codes
-
-
-async def get_code_from_programguide() -> List[str]:
-    try:
-        async with aiohttp.ClientSession() as session, session.get(
-            "https://progameguides.com/genshin-impact/genshin-impact-codes/"
-        ) as response:
-            html = await response.text()
-
-        soup = BeautifulSoup(html, "html.parser")
-        lis = soup.select("div.entry-content li:not(:has(a)):has(strong)")
-
-        codes = [
-            li.strong.text.strip() for li in lis if li.strong.text.strip().isupper() # noqa
-        ]
-
-    except Exception as e:
-        print(f"Error in get_code_from_progameguides: {e}")
-        return []
-
-    return codes
-
-
-async def get_code_from_gipn() -> List[str]:
-    async with aiohttp.ClientSession() as session, session.get(
-        "https://raw.githubusercontent.com/ataraxyaffliction/gipn-json/main/gipn-update.json" # noqa
-    ) as response:
-        json_content = await response.text()
-        json_content = json.loads(json_content)
-
-        codes = json_content.get("CODES", {})
-    available_codes = [
-        {"code": code["code"], "description": code["reward"]}
-        for code in codes
-        if not code.get("is_expired", True)
-    ]
-    return available_codes
-
-
-async def get_code_from_pockettactics() -> List[dict]:
-    codes = []
-    try:
-        async with aiohttp.ClientSession() as session, session.get(
-            "https://www.pockettactics.com/genshin-impact/codes"
-        ) as response:
-            html = await response.text()
-
-        soup = BeautifulSoup(html, "html.parser")
-        entries = soup.select(
-            "div.entry-content > ul > li:not(:has(a)) > strong:not(:has(a))"
-        )
-
-        for entry in entries:
-            code = entry.get_text().strip()
-            if code and code == code.upper():
-                description = (
-                    entry.parent.get_text().split("–")[1].replace("(new!)", "").strip() # noqa
-                )
-                codes.append({"code": code, "description": description})
-
-    except Exception as e:
-        print(f"Error in get_code_from_pockettactics: {e}")
-        return []
-
-    return codes
 
 
 async def get_code_from_eurogamer() -> List[dict]:
@@ -128,23 +44,20 @@ async def root():
 
 @app.get("/codes")
 async def read_codes() -> List[dict]:
-    pocket_codes, program_codes, gipn, games_radar, euro_gamer = await asyncio.gather(
-        get_code_from_pockettactics(),
-        get_code_from_programguide(),
-        get_code_from_gipn(),
-        get_code_from_gamesrader(),
+    euro_gamer = await asyncio.gather(
         get_code_from_eurogamer(),
     )
 
     old_codes = ["GENSHINGIFT", "XBRSDNF6BP4R", "FTRUFT7AT5SV"]
 
-    all_codes = gipn + pocket_codes + program_codes + games_radar + euro_gamer
+    all_codes = [code for sublist in euro_gamer for code in sublist]
+
     unique_codes = {code["code"]: code for code in all_codes}.values()
 
     filtered_codes = [
         code
         for code in unique_codes
-        if code["code"] not in old_codes
+        if code["code"] not in old_codes and code["description"] != ""
     ]
 
     if len(filtered_codes) == 0:
